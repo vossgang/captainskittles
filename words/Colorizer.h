da3d8c62d47ecdd514eef54022ee30e83bdc0cde
Brian Radebaugh <rivukis@gmail.com>
2014-05-27 15:46:13 -0700

    add Colorizer Class with method to pick color based on time

---
+++ words/Colorizer.h
@@ -0,0 +1,13 @@
+//
+//  Colorizer.h
+//  words
+//
+//  Created by Brian Radebaugh on 5/27/14.
+//  Copyright (c) 2014 Christopher Cohen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Colorizer : NSObject
+
+@end
---
+++ words/Colorizer.m
@@ -0,0 +1,13 @@
+//
+//  Colorizer.m
+//  words
+//
+//  Created by Brian Radebaugh on 5/27/14.
+//  Copyright (c) 2014 Christopher Cohen. All rights reserved.
+//
+
+#import "Colorizer.h"
+
+@implementation Colorizer
+
+@end

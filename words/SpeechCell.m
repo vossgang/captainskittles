ae0a86af2e91a0dc6f72b3ed01a37cd04d1c05c4
vossgang <vossgang@gmail.com>
2014-05-27 09:54:20 -0700

    did stuff

---
+++ words/SpeechCell.m
@@ -0,0 +1,31 @@
+//
+//  SpeechCell.m
+//  words
+//
+//  Created by Matthew Voss on 5/26/14.
+//  Copyright (c) 2014 Christopher Cohen. All rights reserved.
+//
+
+#import "SpeechCell.h"
+
+@implementation SpeechCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
